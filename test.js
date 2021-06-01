570cc400a5cb4744cb6365b2ce5a148dd5761414
Kim-donguk <dkfnfn9871@gmail.com>
2021-06-01 10:54:57 +0900

    mail program

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
 console.log("Hello World")
+
+const nodemailer = require("nodemailer");
+const email = {
+
+    "host" : "",
+    "post" : "",
+    "secure": false,
+    "auth" : {
+        "user" : "",
+        "pass" : ""
+    }
+};
+
+const send = async (option) => {
+    nodemailer.createTransport(email)
+};
